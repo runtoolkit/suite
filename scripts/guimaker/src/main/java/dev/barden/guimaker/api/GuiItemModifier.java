0edcee56ee8f9bad0ee7fdd456e158996fe93a51
IronCrest <234182831+IronCrest-sudo@users.noreply.github.com>
2026-08-10 21:50:47 +0300

    Delete scripts/guimaker directory

--- scripts/guimaker/src/main/java/dev/barden/guimaker/api/GuiItemModifier.java
+++
@@ -1,9 +0,0 @@
-package dev.barden.guimaker.api;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
-
-@FunctionalInterface
-public interface GuiItemModifier {
-    ItemStack apply(ServerPlayerEntity player, ItemStack stack);
-}
