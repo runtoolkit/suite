2dfc51d4113b1f0bf963525546a1d01db2f0bc8e
Legends11 <235496468+tickwarden@users.noreply.github.com>
2026-08-11 15:50:56 +0300

    Modify lint.sh to adjust ignored paths
    Updated IGNORE_PATHS to include specific function files.

--- .github/workflows/lint.sh
+++ .github/workflows/lint.sh
@@ -9,9 +9,10 @@ echo "::endgroup::"
 IGNORE_PATHS=(
     "packs/inv_gui"
     "packs/macroEngine-dp/26_1"
-    "packs/dataLib-dp/datapacks/dataLib"
-    "packs/dataLibDatapackOnly",
-    "packs/cmdTunnel-datapack"
+    "packs/dataLibDatapackOnly/data/datalib/function/world/get_time.mcfunction"
+    "packs/dataLibDatapackOnly/data/datalib/function/world/time_phase.mcfunction"
+    "packs/dataLibDatapackOnly/data/datalib/function/world/time_phase.mcfunction"
+    "packs/cmdTunnel-datapack/data/*/functions/load.mcfunction"
 )
 
 echo "::group::🚫 Ignoring paths"
